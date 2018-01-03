39c5fc0f55f6233f61532c43b99d08b660754753
Aidan Wang <ajwang@berkeley.edu>
2018-01-02 18:16:53 -0800

    added name, assignment, and time fields to Task

--- src/Task.java
+++ src/Task.java
@@ -4,4 +4,55 @@
  */
 public class Task {
 
+    /* CONSTRUCTORS */
+
+    public Task(String name, Assignment assignment, int time) {
+        _name = name;
+        _assignment = assignment;
+        _time = time;
+    }
+
+    /* ACCESSORS */
+
+    /**
+     * Accessor for _name.
+     * @return
+     */
+    public String get_name() {
+        return _name;
+    }
+
+    /**
+     * Accessor for _assignment.
+     * @return
+     */
+    public Assignment get_assignment() {
+        return _assignment;
+    }
+
+    /**
+     * Accessor for _time.
+     * @return
+     */
+    public int get_time() {
+        return _time;
+    }
+
+    /* FIELDS */
+
+    /**
+     * Name of task.
+     */
+    private String _name;
+
+    /**
+     * The assignment which this task is part of.
+     */
+    private Assignment _assignment;
+
+    /**
+     * Time needed for completion of task.
+     */
+    private int _time;
+
 }
